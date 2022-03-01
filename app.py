5d23891290a023a6034e572cd900b6dcefd3e597
mrBELLAZIA <73542412+mrBELLAZIA@users.noreply.github.com>
2022-03-01 10:32:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,17 @@ gmaps = googlemaps.Client(key="AIzaSyAw2wtEBHUuMMik8_jq_TixPz9B5mD0Xws")
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/findWaysPoints/<float:lat>/<float:lng>/<int:limite>',methods=['GET'])
-def findWaysPoints(lat,lng,limite):
-    response = requests.get("https://opendata.reseaux-energies.fr/api/records/1.0/search/?dataset=bornes-irve&q=&sort=-dist&facet=region&geofilter.distance="+str(lng)+"%2C"+str(lat)+"%2C"+str(limite))
-    data = [response.json()["records"]] #[0]["fields"]["ylatitude"] , [0]["fields"]["xlongitude"]
-    return jsonify(data)
 
+class findWaysPoints(Resource):
+    def get(self):
+        lat = float(requests.args.get('lat'))
+        lng = float(requests.args.get('lng'))
+        limite = int(requests.args.get('limite'))
+        response = requests.get("https://opendata.reseaux-energies.fr/api/records/1.0/search/?dataset=bornes-irve&q=&sort=-dist&facet=region&geofilter.distance=" + str(lng) + "%2C" + str(lat) + "%2C" + str(limite))
+        data = [response.json()["records"]]  # [0]["fields"]["ylatitude"] , [0]["fields"]["xlongitude"]
+        return jsonify(data)
+
+api.add_resource(findWaysPoints,'/findWaysPoints')
 
 if __name__ == '__main__':
     print("wollah ca marche")
